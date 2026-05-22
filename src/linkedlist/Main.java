548a57d765da6c53d40eb8697b900252ca3c8e1b
WARHE8D <59991459+WARHE8D@users.noreply.github.com>
2026-05-22 13:59:46 -0600

    delete insert ll

--- src/linkedlist/Main.java
+++ src/linkedlist/Main.java
@@ -38,11 +38,20 @@ public class Main {
     public static void TraverseLL(Node n){
         if(n==null) return;
         StringBuilder sb = new StringBuilder();
-        while(n.next!=null){
+        while(n!=null){
             sb.append(n.data).append(" ");
             n=n.next;//impt
         }
         System.out.println(sb);
+    }
+
+    public static int LLSize(Node n){
+        int count = 0;
+        while(n!=null){
+            n=n.next;//impt
+            count++;
+        }
+        return count;
     }
 
 
@@ -55,6 +64,62 @@ public class Main {
         }
         return null;
     }
+
+    //Insert Head
+    public static Node InsertHead(Node n,int val){
+        return new Node(val,n);//new Node(new value, ptr to next node)
+    }
+
+    public static Node InsertTail(Node n,int val){
+        if(n==null) return null;
+        Node mover = n;
+        while(mover.next!=null){
+            mover = mover.next;
+        }
+        mover.next = new Node(val);
+        return n;
+    }
+
+
+    //Delete LL Head
+    // returning n.next moves the head to the second node,
+    // making the first node unreachable and eligible for 'garbage collection'
+    public static Node DeleteHead(Node n){
+        if(n==null || n.next==null) return null;
+        return n.next;
+    }
+
+    //delete tail
+    public static Node DeleteTail(Node n){
+        if(n==null || n.next == null) return null;
+
+        // mover is used only for traversal so the head pointer 'n' remains unchanged
+        Node mover = n;
+        while(mover.next.next != null){// scans until second last node
+            mover=mover.next;
+        }
+        mover.next = null;//moves and nulls the Node n
+        return n;
+    }
+
+    public static Node DeleteVal(Node n,int value){
+        if(n==null || n.next == null) return n;
+        if(n.data == value) return DeleteHead(n);
+        Node temp =  n;
+        Node prev = n;
+        while(temp!=null){
+            if(temp.data==value){
+                prev.next = temp.next;
+                return n;
+            }
+            prev = temp;
+            temp=temp.next;
+        }
+        return n;
+    }
+
+
+
 
 
     public static void main(String[] args) {
@@ -71,5 +136,36 @@ public class Main {
 //        TraverseLL(n);
         Node target = SearchLL(n,4);
         System.out.println(target != null ? "Search: "+target.data : 0);
+
+        //insert
+//        Node insert = InsertLL(n,new Node(12),-1);
+
+        TraverseLL(n);
+//        TraverseLL(insert);
+
+//        delete element
+        Node deletedVal = DeleteVal(n,3);
+        System.out.println("Delete Value:");
+        TraverseLL(deletedVal);
+        //delete Head
+        Node deleteHead = DeleteHead(n);
+        System.out.println("Delete Head:");
+        TraverseLL(deleteHead);
+
+
+        //delete Tail
+        Node deleteTail = DeleteTail(n);
+        System.out.println("Delete Tail:");
+        TraverseLL(deleteTail);
+
+        //insert val head
+//        Node insertHead
+        Node insertHead = InsertHead(n,9);
+        System.out.println("Insert Head Val:");
+        TraverseLL(insertHead);
+        //Node insertTail
+        Node insertTail = InsertTail(n,19);
+        System.out.println("Insert Tail Val:");
+        TraverseLL(insertTail);
     }
 }
