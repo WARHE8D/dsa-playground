07af0a03c2cfeac7ed64092982cd6fb422e8e805
WARHE8D <59991459+WARHE8D@users.noreply.github.com>
2026-05-21 19:04:01 -0600

    traverse and search Linked list

--- src/linkedlist/Main.java
+++ src/linkedlist/Main.java
@@ -21,7 +21,9 @@ public class Main {
         Node head = new Node(arr[0]);
         Node ptr = head;
         for(int i =1; i<arr.length;i++){
-            ptr.next = new Node(arr[i]);
+            Node element = new Node(arr[i]);
+            ptr.next = element;
+            ptr = element;
         }
         return head;
     }
@@ -33,7 +35,26 @@ public class Main {
 //        }
 //    }
 
+    public static void TraverseLL(Node n){
+        if(n==null) return;
+        StringBuilder sb = new StringBuilder();
+        while(n.next!=null){
+            sb.append(n.data).append(" ");
+            n=n.next;//impt
+        }
+        System.out.println(sb);
+    }
 
+
+    public static Node SearchLL(Node n, int target){
+        if(n==null) return null;
+
+        while(n.next!=null){
+            if(n.data==target) return n;
+            n=n.next;
+        }
+        return null;
+    }
 
 
     public static void main(String[] args) {
@@ -46,5 +67,9 @@ public class Main {
         int[] arr = {1,5,3,6,3,4,6};
         Node n = ArrayToLinkedList(arr);
         System.out.println(n != null ? n.data : null);
+
+//        TraverseLL(n);
+        Node target = SearchLL(n,4);
+        System.out.println(target != null ? "Search: "+target.data : 0);
     }
 }
